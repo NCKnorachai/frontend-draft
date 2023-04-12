7ebfcdfae35fa1b423670b2d02e9bf9f5ecf1a53
Mat-tiw <Tiweymain@gmail.com>
2023-04-12 12:01:45 +0700

    fixed main and add few components

--- src/components/Main-page.jsx
+++ src/components/Main-page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/member.css";
 import Book from "../asset/icon/book.png";
 import People from "../asset/icon/people.png";
@@ -8,45 +8,65 @@ import Menu from "../asset/icon/menu.png";
 import Chat from "../asset/icon/chat.png";
 import docs from "../asset/icon/google-docs.png";
 import Logout from "../asset/icon/logout.png";
+import Members from "./Members";
+import Placeholder from "./Placeholder";
 
 export default function Mainpage() {
+  const [PagesNum, setPagesNum] = useState(0);
+  function renderSwitch(PagesNum){
+    switch(PagesNum){
+      case 1:
+        return <Members />;
+      case 2:
+        return <Placeholder />;
+      case 3:
+        return <Members />
+      default:
+        <Members/>;
+        break;
+    };
+  }
   return (
-    <div className="member-sidebar">
-      <ul className="member-sisdebar-menu">
-        <h1 className="member-sisdebar-menu-texthean">ระบบนัดหมาย</h1>
-        <li className="member-sisdebar-textmenu-report-summary">
-          <img src={Book} alt="" />
-          <p> สรุปรายงาน</p>
-        </li>
-        <li className="member-sisdebar-textmenu-officer">
-          <img src={People} alt="" />
-          <p>เจ้าหน้าที่</p>
-        </li>
-        <li className="member-sisdebar-textmenu-member">
-          <img src={Business} alt="" />
-          <p>สมาชิก</p>
-        </li>
-        <li className="member-sisdebar-textmenu-assign">
-          <img src={Calendar} alt="" />
-          <p>จัดการนัดหมาย</p>
-        </li>
-        <li className="member-sisdebar-textmenu-service-group">
-          <img src={Menu} alt="" />
-          <p>กลุ่มงานบริการ</p>
-        </li>
-        <li className="member-sisdebar-textmenu-automatic-message">
-          <img src={Chat} alt="" />
-          <p>ข้อความอัตโนมัติ</p>
-        </li>
-        <li className="member-sisdebar-textmenu-Instruction-Manual">
-          <img src={docs} alt="" />
-          <p>คู่มือการใช้งาน</p>
-        </li>
-        <li className="member-sisdebar-textmenu-logout">
-          <img src={Logout} alt="" />
-          <p>ออกจากระบบ</p>
-        </li>
-      </ul>
+    <div className="wrapper">
+      <div className="member-sidebar">
+        <ul className="member-sisdebar-menu">
+          <h1 className="member-sisdebar-menu-texthean">ระบบนัดหมาย</h1>
+          <li className="member-sisdebar-textmenu-report-summary">
+            <img src={Book} alt="" />
+            <p onClick={() => setPagesNum(1)}> สรุปรายงาน</p>
+          </li>
+          <li className="member-sisdebar-textmenu-officer">
+            <img src={People} alt="" />
+            <p onClick={() => setPagesNum(2)}>เจ้าหน้าที่</p>
+          </li>
+          <li className="member-sisdebar-textmenu-member">
+            <img src={Business} alt="" />
+            <p>สมาชิก</p>
+          </li>
+          <li className="member-sisdebar-textmenu-assign">
+            <img src={Calendar} alt="" />
+            <p>จัดการนัดหมาย</p>
+          </li>
+          <li className="member-sisdebar-textmenu-service-group">
+            <img src={Menu} alt="" />
+            <p>กลุ่มงานบริการ</p>
+          </li>
+          <li className="member-sisdebar-textmenu-automatic-message">
+            <img src={Chat} alt="" />
+            <p>ข้อความอัตโนมัติ</p>
+          </li>
+          <li className="member-sisdebar-textmenu-Instruction-Manual">
+            <img src={docs} alt="" />
+            <p>คู่มือการใช้งาน</p>
+          </li>
+          <li className="member-sisdebar-textmenu-logout">
+            <img src={Logout} alt="" />
+            <p>ออกจากระบบ</p>
+          </li>
+        </ul>
+      </div>
+      <h1>test render</h1>
+      {renderSwitch(PagesNum)}
     </div>
   );
 }
---
+++ src/components/Placeholder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Placeholder = () => {
+  return (
+    <div>
+      placeholder placeholder
+    </div>
+  )
+}
+
+export default Placeholder
